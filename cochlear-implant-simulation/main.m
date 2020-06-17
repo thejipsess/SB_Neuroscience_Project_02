a1e503709b2d54d4683b9db911f6c1eb411fc30a
Seditoir <59647204+Seditoir@users.noreply.github.com>
2020-06-17 12:42:09 +0200

    made a loop which cycles throught the audio files
    made a loop which cycles throught the audio files, in the 'In' folder and generates the corrosponding wav files

--- cochlear-implant-simulation/main.m
+++ cochlear-implant-simulation/main.m
@@ -1,32 +1,49 @@
 clear; close all; clc; 
 
-% Set settings
-FS = 16e3;
-filetype = '.mp3';
-filename = 'Japanese jo';
+% setting audio in map
 
-% Load  the audio file
-[x,fs] = audioread(['Audio/in/',filename, filetype]);
-
-% Resample the audio
-x = resample(x, FS, fs);
-
-% Convert audio to the cochlear implant signal
-y = vocoder(x, FS, 22, 50, 'NOISE', 1);
+directory = dir('Audio/in/*.mp3');
+numfiles = length (directory);
 
 
-% play sound
-sound(y, FS)
+for i=1:numfiles
 
-% Plot the new audio signal
-figure;
-plot((1:length(y))/FS, y);
-xlabel('Time (second)')
-title('Vocoded signal')
 
-% Normalise the sound signal between -1 and 1 since the audiowriter cannot
-% handle values outside this range.
-y2 = normalize(y, 'range', [-1 1]);
+    % Set settings
+      FS = 16e3;
+    % filetype = '.mp3';
+    % filename = 'beethoven_fur_elise_orig';
+    
+    % getting file name withouth extentsion
+    file = directory(i,1);
+    file = file.name
+    [filepath,name,ext] = fileparts(file)
 
-% Export the converted audio file
-audiowrite(['Audio/out/',filename,'.wav'], y2, FS);
+    % Load  the audio file
+    [x,fs] = audioread(['Audio/in/',name,ext]);
+    
+    % Resample the audio
+    x = resample(x, FS, fs);
+
+    % Convert audio to the cochlear implant signal
+        y = vocoder(x, FS, 22, 50, 'NOISE', 1);
+
+
+    % play sound commented out
+    % sound(y, FS)
+
+    % Plot the new audio signal
+    figure;
+    plot((1:length(y))/FS, y);
+    xlabel('Time (second)')
+    title('Vocoded signal')
+
+    % Normalise the sound signal between -1 and 1 since the audiowriter cannot
+    % handle values outside this range.
+    y2 = normalize(y, 'range', [-1 1]);
+
+    % Export the converted audio file
+    filename = sprintf(name)
+    audiowrite(['Audio/out/',filename,'.wav'], y2, FS);
+
+end
