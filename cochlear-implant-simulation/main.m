45348460c662e52150b50df1460adb9b62bc23cc
Jip de Kok <jipdekok@hotmail.com>
2020-06-19 20:23:06 +0200

    Major overhaul of vocoder and main file
    Extensive changes have been applied to both the main and vocoder file
    
    - the .mat files are now loaded from within the vocoder, currently only .mat files are properly supported.
    - the output file is now a cell array containing both all the vocoded audio signals and the channel architecture used to generate the audio signals.
    - the .wav output files have been put in a wav folder to maintain overview of the Audio/out folder

--- cochlear-implant-simulation/main.m
+++ cochlear-implant-simulation/main.m
@@ -1,5 +1,12 @@
 clear; close all; clc; 
 
+% Set the general settings:
+audio = 'Audio/matin/Stim288.mat';
+optimise = 0;
+iterations = 1;
+
+
+
 prompt = 'Do you have a .mat file? y/n:';
 Userinput = input(prompt,'s');
 USerinput = convertCharsToStrings(Userinput)
@@ -30,10 +37,10 @@ if strcmp(Userinput,'n')
         x = resample(x, FS, fs);
 
         % Convert audio to the cochlear implant signal
-        %y = vocoder(x, FS, 22, 50, 'NOISE', 1);
+        y = vocoder(x, FS, 22, '22-electrodes', 240, 'NOISE', 1);
         
-        % New implementation of the vocoder
-        [y, config] = vocoder(sound, FS, 22, '22-electrodes', 240, 'NOISE', 0, 1, 100);
+        % New implementation of the vocoder (DOESN'T WORK YET WITH OPTIMISATION)
+        %[y, config] = vocoder(sound, FS, 22, '22-electrodes', 240, 'NOISE', 0, 1, 100);
 
 
         % play sound commented out
@@ -61,49 +68,17 @@ if strcmp(Userinput,'n')
 elseif strcmp(Userinput,'y')
     % Set settings
     FS = 16e3;
-        
-    % load in the work space
-    load('Audio/matin/Stim288.mat')
 
-    % settings for the for loop
-    numfiles = length (labels);
-    stim_CI = zeros(length(labels),fs);
-    
-    
-    for i=1:numfiles
-        %selecting sounds
-        soundnumber= i;
-        sound = stim(soundnumber,:);
-    
-        % Convert audio to the cochlear implant signal
-        %y = vocoder(sound, fs, 22, 50, 'NOISE', 0);
-        
-        % New implementation of the vocoder
-        [y, config] = vocoder(sound, FS, 22, '22-electrodes', 240, 'NOISE', 0, 1, 5);
-    
-        % Plot the new audio signal commented out
-        % figure;
-        % plot((1:length(y))/fs, y);
-        % xlabel('Time (second)');
-        % title('Vocoded signal');
+    % New implementation of the vocoder
+    [stim_CI, config] = vocoder(audio, FS, 8, 'default', 240, 'NOISE', 0, optimise, iterations);
 
-        % Normalise the sound signal between -1 and 1 since the audiowriter cannot
-        % handle values outside this range.
-        y2 = normalize(y, 'range', [-1 1]);
-        
-        % writing out the audio
-        
-        % name= labels{i};
-        % chr = mat2str(i);
-        % audiowrite(['Audio/matout/',name,chr,'.wav'], y2, fs)
-        
-        stim_CI(i,:) = y2;
-        
-        %counter
-        count = i
-    end
-    
-    %save('Stim_CI','fs','stim_CI','labels','stim')
+    % writing out the audio
+
+    % name= labels{i};
+    % chr = mat2str(i);
+    % audiowrite(['Audio/matout/',name,chr,'.wav'], y2, fs)
+
+    save('Audio/out/stim_CI_8Channels','fs','stim_CI','labels')
     
 else 
     disp("error wrong input")    
--- cochlear-implant-simulation/vocoder.m
+++ cochlear-implant-simulation/vocoder.m
@@ -1,4 +1,4 @@
-function [y, cf_rand] = vocoder(x, Fs, n_channels, channel_spacing, cutoff, vocoder_type, verbose, optimise, iterations)
+function [CI_signals, cf_rand] = vocoder(x, Fs, n_channels, channel_spacing, cutoff, vocoder_type, verbose, optimise, iterations)
 % Implementation of cochlear implant simulation, referred to as vocoder.
 % The first argument is the signal.
 % The second argument is the sampling Fs (preferred 16Khz)
@@ -22,6 +22,20 @@ function [y, cf_rand] = vocoder(x, Fs, n_channels, channel_spacing, cutoff, voco
 
 % Start timer
 tic
+
+if ischar(x)
+    % Check if the audio input is specified as a .mat files
+    if strcmp(x(end-3:end), '.mat')
+        % Load the .mat file
+        load(x)
+        
+        x = transpose(stim);
+        
+        disp('loaded .mat file succesfully')
+        % Still need to incorporate and else statement to handle individual
+        % files or folders filled with audio files.
+    end
+end
 
 % If not all inputs are defined then set missing inputs to their defaults.
 switch nargin
@@ -66,17 +80,7 @@ switch nargin
         end
 end
 
-% Make sure the signal is a column vector
-if( size(x,2)>size(x,1) )
-    x = x';
-end
-x = x(:,1);
-
 npts=length(x);
-
-% Apply high-pass pre-emphasis filter
-pre=0.9378;
-xx=filter([1 -pre], 1, x)';
 
 % Center Freq. based on number of channels. These values are taken from
 % Dorman, Loizou, Rainey, (1997). Speech intelligibility as a function of the number of channels
@@ -87,7 +91,6 @@ Wn = space_channels(n_channels, Fs, channel_spacing);
 
 if optimise
     verbose = 0;
-
 end
 
 % Randomise channel placement for optimisation
@@ -98,103 +101,124 @@ else
     iterations = 1;
 end
 
-% Initialise y, which will contain all processed CI audio signals
-y = NaN(length(x), iterations);
-
-index = 1;
-for iter = 1:iterations
-    if optimise
-        Wn = Wn_rand(:,index:index+1);
-    end
-    % GeneFs lowpass filter coefficients (for envelope extraction):
-     fc=cutoff /(Fs/2);
-     % The butter-worth filter create a frequency response as flat as possible
-     % in the pass band region. 
-    [blp,alp]=butter(2,fc,'low'); % geneFs filter coefficients
-
-    % GeneFs noise carrier only for noise vocoders
-    if( strcmp(vocoder_type, 'NOISE') )
-        noise = rand( length(x),1 );
-        noise = noise(:);
-    end
-    
-    % Initialse vocoded_x
-    vocoded_x=zeros(npts,1);
-
-    for i=1:n_channels
-
-        %     Find the filter coefficients for each bandpass filter
-        [b,a] = butter(4,Wn(i,:));
-
-        if(verbose)
-            [h,f]=freqz(b,a,1024,Fs);        
-            figure(1);
-            plot(f,20*log10(abs(h+eps)),'LineWidth',2);
-            hold on;
-            min_f = min(Wn(:,1))*Fs/2 - 50;
-            axis([min_f Fs/2 -6 0.5]);
-            x_ticks = round(logspace(log10(min_f), log10(Fs/2), n_channels));
-            set(gca,'XScale','log', 'XTick', x_ticks);                  
-            xlabel('Frequency (Hz)');
-            ylabel('Filter gain (dB)');
-            title('Band-pass filters transfer function');
-            grid on;
-        end
-
-        % now filter the input waveform using filter #i
-        filtwav = filtfilt(b,a,xx)';
-
-        %     Half-wave rectification
-        filtwav(filtwav<0) = 0;
-
-        %     Filter the band-passed filtered signal to extract its envelope
-        %     (Overall shape)
-        envelope=filter(blp,alp,filtwav);
-        envelope = envelope(:);
-
-        %     If noise vocoder is selected, then multiply the envelope with the
-        %     noise carrier.
-        %    Basically, we modulate the noise by the envelope
-        if(strcmp(vocoder_type, 'NOISE'))
-            if length(envelope) > length(noise)
-                envelope(length(noise)+1:length(envelope))=[];
-            end
-
-            if length(noise) > length(envelope)
-                noise(length(envelope)+1:length(noise))=[];
-            end
-
-            source = noise./(max(abs(noise)));
-            disp(index)
-            disp(iter)
-            disp(i)
-            fn=filtfilt(b,a,envelope.*source);
-
-        else
-            %     If tone vocoder is selected, then multiply the envelope with a
-            %     tone carrier.
-            %    Basically, we modulate the tone by the envelope
-
-            %     Tone with freq. at the center of the band-pass filter
-            f = exp(mean(log(Wn(i,:)))) * (Fs/2);
-            tone=sin(2*pi*(1:length(envelope))*f/Fs)';
-            tone = tone(:);
-            fn = envelope.*tone;
-
-        end
-
-        % sum bands with equal gain in each channel
-        vocoded_x = vocoded_x + fn;
-        
-    end
-    y(:, iter) = vocoded_x;
-    index = index + 2;
+% Initialise the CI_signals variable which will store all the different
+% channel configurations that are being used, and with them stores all the
+% CI converted audio signals.
+CI_signals = cell(iterations, 2);
+for arch = 1:iterations
+    CI_signals{arch,2} = cf_rand(:,arch);
 end
-    % Scale output waveform to have same rms as original
-    for i = 1:size(y, 2)
-        y(:,i) = y(:,i) * (rms(x)/rms(y(:,i)));
-    end
 
-    % Stop timer
-    toc
+% Loop though all audio segments in x (as = Audio Segment)
+for as = 1:size(x, 2)
+    % Initialise y, which will contain all processed CI audio signals
+    y = NaN(length(x(:, as)), iterations);
+
+    % Apply high-pass pre-emphasis filter
+    pre=0.9378;
+    xx=filter([1 -pre], 1, x(:, as))';
+
+    index = 1;
+    for iter = 1:iterations
+        if optimise
+            Wn = Wn_rand(:,index:index+1);
+        end
+        % GeneFs lowpass filter coefficients (for envelope extraction):
+         fc=cutoff /(Fs/2);
+         % The butter-worth filter create a frequency response as flat as possible
+         % in the pass band region. 
+        [blp,alp]=butter(2,fc,'low'); % geneFs filter coefficients
+
+        % GeneFs noise carrier only for noise vocoders
+        if( strcmp(vocoder_type, 'NOISE') )
+            noise = rand( length(x(:, as)),1 );
+            noise = noise(:);
+        end
+
+        % Initialse vocoded_x
+        vocoded_x=zeros(npts,1);
+
+        for i=1:n_channels
+
+            %     Find the filter coefficients for each bandpass filter
+            [b,a] = butter(4,Wn(i,:));
+
+            if(verbose)
+                [h,f]=freqz(b,a,1024,Fs);        
+                figure(1);
+                plot(f,20*log10(abs(h+eps)),'LineWidth',2);
+                hold on;
+                min_f = min(Wn(:,1))*Fs/2 - 50;
+                axis([min_f Fs/2 -6 0.5]);
+                x_ticks = round(logspace(log10(min_f), log10(Fs/2), n_channels));
+                set(gca,'XScale','log', 'XTick', x_ticks);                  
+                xlabel('Frequency (Hz)');
+                ylabel('Filter gain (dB)');
+                title('Band-pass filters transfer function');
+                grid on;
+            end
+
+            % now filter the input waveform using filter #i
+            filtwav = filtfilt(b,a,xx)';
+
+            %     Half-wave rectification
+            filtwav(filtwav<0) = 0;
+
+            %     Filter the band-passed filtered signal to extract its envelope
+            %     (Overall shape)
+            envelope=filter(blp,alp,filtwav);
+            envelope = envelope(:);
+
+            %     If noise vocoder is selected, then multiply the envelope with the
+            %     noise carrier.
+            %    Basically, we modulate the noise by the envelope
+            if(strcmp(vocoder_type, 'NOISE'))
+                if length(envelope) > length(noise)
+                    envelope(length(noise)+1:length(envelope))=[];
+                end
+
+                if length(noise) > length(envelope)
+                    noise(length(envelope)+1:length(noise))=[];
+                end
+
+                source = noise./(max(abs(noise)));
+                fn=filtfilt(b,a,envelope.*source);
+
+            else
+                %     If tone vocoder is selected, then multiply the envelope with a
+                %     tone carrier.
+                %    Basically, we modulate the tone by the envelope
+
+                %     Tone with freq. at the center of the band-pass filter
+                f = exp(mean(log(Wn(i,:)))) * (Fs/2);
+                tone=sin(2*pi*(1:length(envelope))*f/Fs)';
+                tone = tone(:);
+                fn = envelope.*tone;
+            end
+
+            % sum bands with equal gain in each channel
+            vocoded_x = vocoded_x + fn;
+
+        end
+        y(:, iter) = vocoded_x;
+        index = index + 2;
+    end
+        
+        for i = 1:size(y, 2)
+            % Scale output waveform to have same rms as original
+            y(:,i) = y(:,i) * (rms(x(:, as))/rms(y(:,i)));
+            
+            % Normalise the sound signal such that it does not surpass the
+            % extreme valuse of the input sound. This ensure that the
+            % loudness of both signals is comparable.
+            y(:,i) = normalize(y(:,i), 'range', [min(x(:, as)) max(x(:, as))]);
+            
+            % Store the vocoded audio signal in the CI_signals variable
+            CI_signals{i,1}(:,as) = y(:,i);
+        end
+end
+
+% Stop timer
+toc
+
 end
