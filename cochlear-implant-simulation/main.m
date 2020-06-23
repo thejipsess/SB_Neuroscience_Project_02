21c3be2b2bc725676e8680e433df6a7a373ba2f8
Jip de Kok <jipdekok@hotmail.com>
2020-06-23 11:39:47 +0200

    No changes

--- cochlear-implant-simulation/main.m
+++ cochlear-implant-simulation/main.m
@@ -7,7 +7,7 @@ iterations = 1;
 FS = 16e3;
 
 % New implementation of the vocoder
-[stim_CI, config, labels] = vocoder(audio, FS, 22, '22-electrodes',160, 'NOISE', 0, optimise, iterations);
+[stim_CI, config, labels] = vocoder(audio, FS, 10, '10-electrodes',160, 'NOISE', 0, optimise, iterations);
 
 % Save output
 save('Audio/out/stim_CI_22Channels_160','FS','stim_CI','labels')
